2e31b5b92380b6998dd6220fb719754c3eb81ccc
asd <asdasdsda@asdasd>
2015-02-18 13:30:44 +0100

    Why do you do this github

--- DLP/src/Main.java
+++ DLP/src/Main.java
@@ -3,8 +3,16 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) {
-		Yylex lex = new Yylex(System.in);
+		Lexico lex = new Lexico(System.in);
+
+		int token;
+
 		try {
+
+			while ((token = lex.yylex()) != 0) {
+				System.out.println("Token: " + token);
+			}
+
 			lex.yylex();
 		} catch (IOException e) {
 			e.printStackTrace();
