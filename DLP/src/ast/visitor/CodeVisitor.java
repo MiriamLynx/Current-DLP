166a72bce61e6375cd816ef0859b7b7168374549
Miriam Abalo Fernández <mirylynx@gmail.com>
2015-04-15 18:39:37 +0200

    More thingys

--- DLP/src/ast/visitor/CodeVisitor.java
+++ DLP/src/ast/visitor/CodeVisitor.java
@@ -1,13 +1,34 @@
 package ast.visitor;
 
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 import ast.Programa;
 
 public class CodeVisitor extends AbstractVisitor {
 
+	private String file;
+	private PrintWriter writer;
+	private Map<String, String> operation = new HashMap<String, String>();
+
+	public CodeVisitor(String file, PrintWriter writer) {
+		this.file = file;
+		this.writer = writer;
+		operation.put("+", "add");
+		operation.put("-", "sub");
+		operation.put("*", "mul");
+		operation.put("/", "div");
+	}
+
 	public Object visit(Programa programa) {
-		System.out.println("#source\"" + "NombreFicheroFuente" + "\"");
-		System.out.println("callmain");
-		System.out.println("halt");
+		out("#source\"" + file + "\"");
+		out("callmain");
+		out("halt");
 		return super.visit(programa);
+	}
+
+	private void out(String text) {
+		writer.println(text);
 	}
 }
